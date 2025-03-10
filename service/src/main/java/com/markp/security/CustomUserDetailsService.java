9fb420e4cc46387747609fe224af1df9bd338d69
markpollente <smertkhod@gmail.com>
2025-03-10 12:19:30 +0800

    Add CORS config; Add roles to claims

--- service/src/main/java/com/markp/security/CustomUserDetailsService.java
+++ service/src/main/java/com/markp/security/CustomUserDetailsService.java
@@ -27,7 +27,11 @@ public class CustomUserDetailsService implements UserDetailsService {
         if (employee == null) {
             throw new UsernameNotFoundException("User not found with email: " + email);
         }
-        return new org.springframework.security.core.userdetails.User(employee.getEmail(), employee.getPassword(),
-                employee.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName())).collect(Collectors.toList()));
+        return new org.springframework.security.core.userdetails.User(
+                employee.getEmail(),
+                employee.getPassword(),
+                employee.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                        .collect(Collectors.toList()));
     }
 }
--- service/src/main/java/com/markp/security/JwtService.java
+++ service/src/main/java/com/markp/security/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtService {
@@ -37,7 +39,7 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
-    private Claims extractAllClaims(String token) {
+    public Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
@@ -51,6 +53,9 @@ public class JwtService {
 
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()));
         return createToken(claims, userDetails.getUsername());
     }
 
